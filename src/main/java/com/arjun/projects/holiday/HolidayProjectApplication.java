eba9765225c5c73fe438775f6fe46b5a5dac63c4
Dat-Boi-Arjun <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2020-11-24 17:15:35 -0800

    Initial commit

---
+++ src/main/java/com/arjun/projects/holiday/HolidayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.arjun.projects.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arjun/projects/holiday/HolidayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.projects.holiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
